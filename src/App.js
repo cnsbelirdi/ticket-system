4ad19048ed1fef82240176297ef6236d40047c6d
asudegultepe <gultepeasude@gmail.com>
2022-05-22 23:12:16 +0300

    Project baseline.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Music from './components/pages/Music';
+import Cinema from './components/pages/Cinema';
+import Stage from './components/pages/Stage';
+import Others from './components/pages/Others';
+import SignUp from './components/pages/SignUp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/music' element={<Music />} />
+                    <Route path='/cinema' element={<Cinema />} />
+                    <Route path='/stage' element={<Stage />} />
+                    <Route path='/others' element={<Others />} />
+                    <Route path='/sign-up' element={<SignUp />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/pages/Cinema.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Cinema() {
+    return <h1 className='cinema'>Cinema</h1>;
+}
