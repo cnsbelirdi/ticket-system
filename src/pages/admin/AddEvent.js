936c181215d9de12b8e929b308252474314ceb9c
cnsbelirdi <cnsbelirdi@gmail.com>
2022-05-30 20:43:23 +0300

    Admin add event page updated.

--- src/pages/admin/AddEvent.js
+++ src/pages/admin/AddEvent.js
@@ -1,79 +1,183 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AddEvent(){
+    const event_types = ["Music", "Cinema","Theater"];
+    const [type ="default", setType] = useState();
     return(
         <div className="bg-admin-grey min-height-65">
             <div className="container d-flex justify-content-center pt-5">
-                <form method="POST" className="min-width-780">
-                    <div className="row">
-                        <div className="col">
-                            <div className="form-group">
-                                <label for="event-type">Event Type</label>
-                                <select className="form-control" name="event-type" id="event-type" required>
-                                    <option>MUSIC</option>
-                                    <option>CINEMA</option>
-                                    <option>STAGE</option>
-                                </select>
-                                <div className="invalid-feedback">Required.</div>
+                <form method="" className="min-width-780">
+                        {
+                            type === "default" ? 
+                            <div className="row">
+                                <div className="col-6">
+                                    <div className="form-group">
+                                        <label htmlFor="event-type">Event Type</label>
+                                        <select value={type} onChange={(e) => setType(e.target.value)} className="form-control text-uppercase" name="event-type" id="event-type" required>
+                                            <option value="default">Select..</option>
+                                            {event_types.map(type => {
+                                                return (
+                                                    <option value={type}>{type}</option>
+                                                );
+                                            })}
+                                        </select>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col">
-                            <div className="form-group">
-                                <label for="event-city">City</label>
-                                <select className="form-control" id="event-city" name="event-city" required>
-                                    <option>ISTANBUL</option>
-                                    <option>ANKARA</option>
-                                    <option>IZMIR</option>
-                                    <option>ANTALYA</option>
-                                    <option>MUGLA</option>
-                                </select>
-                                <div className="invalid-feedback">Required.</div>
+                            :
+                            type === "Music" ?
+                            <>
+                            <div className="row">
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="event-type">Event Type</label>
+                                        <select value={type} onChange={(e) => setType(e.target.value)} className="form-control text-uppercase" name="event-type" id="event-type" required>
+                                            <option value="default">Select..</option>
+                                            {event_types.map(type => {
+                                                return (
+                                                    <option value={type}>{type}</option>
+                                                );
+                                            })}
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="event-city">City</label>
+                                        <select className="form-control" id="event-city" name="event-city" required>
+                                            <option>ISTANBUL</option>
+                                            <option>ANKARA</option>
+                                            <option>IZMIR</option>
+                                            <option>ANTALYA</option>
+                                            <option>MUGLA</option>
+                                        </select>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <div className="form-group">
-                                <label for="event-name">Event Name</label>
-                                <input type="text" className="form-control" id="event-name" name="event-name"/>
+                            <div className="row">
+                                    <div className="col">
+                                        <div className="form-group">
+                                            <label htmlFor="event-name">Event Name</label>
+                                            <input type="text" className="form-control" id="event-name" name="event-name" />
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <div className="form-group">
+                                            <label htmlFor="artist-name">Artist Name</label>
+                                            <input type="text" className="form-control" id="artist-name" name="artist-name" />
+                                        </div>
+                                    </div>
                             </div>
-                        </div>
-                        <div className="col">
-                            <div className="form-group">
-                                <label for="artist-name">Artist Name</label>
-                                <input type="text" className="form-control" id="artist-name" name="artist-name"/>
+                            <div className="row">
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="event-date">Date</label>
+                                        <input type="date" className="form-control" id="event-date" name="event-date" />
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="event-time">Time</label>
+                                        <input type="time" className="form-control" id="event-time" name="event-time" />
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <div className="form-group">
-                                <label for="event-date">Date</label>
-                                <input type="date" className="form-control" id="event-date" name="event-date"/>
+                            <div className="row">
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="description">Description</label>
+                                        <textarea name="description" id="description" className="form-control description" rows="5"></textarea>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col">
-                            <div className="form-group">
-                                <label for="event-time">Time</label>
-                                <input type="time" className="form-control" id="event-time" name="event-time"/>
+                            <div className="row dflex justify-content-end">
+                                <div className="col-2">
+                                    <button type="submit" className="btn bg-orange pl-4 pr-4 radius-10">ADD</button>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <div className="form-group">
-                                <label for="description">Description</label>
-                                <textarea name="description" id="description" className="form-control description" rows="5"></textarea>
+                            </>
+                            :
+                            <>
+                            <div className="row">
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="event-type">Event Type</label>
+                                        <select value={type} onChange={(e) => setType(e.target.value)} className="form-control text-uppercase" name="event-type" id="event-type" required>
+                                            <option value="default">Select..</option>
+                                            {event_types.map(type => {
+                                                return (
+                                                    <option value={type}>{type}</option>
+                                                );
+                                            })}
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="event-city">City</label>
+                                        <select className="form-control" id="event-city" name="event-city" required>
+                                            <option>ISTANBUL</option>
+                                            <option>ANKARA</option>
+                                            <option>IZMIR</option>
+                                            <option>ANTALYA</option>
+                                            <option>MUGLA</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="form-group">
+                                            <label htmlFor="event-name">Event Name</label>
+                                            <input type="text" className="form-control" id="event-name" name="event-name" />
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="row dflex justify-content-end">
-                        <div className="col-2">
-                            <button type="button" class="btn bg-orange pl-4 pr-4 radius-10">ADD</button>
-                        </div>
-                    </div>
+                            <div className="row">
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="event-date">Date</label>
+                                        <input type="date" className="form-control" id="event-date" name="event-date" />
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="event-time">Time</label>
+                                        <input type="time" className="form-control" id="event-time" name="event-time" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="cast">Cast</label>
+                                        <textarea name="cast" id="cast" className="form-control description" rows="5"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="description">Description</label>
+                                        <textarea name="description" id="description" className="form-control description" rows="5"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label htmlFor="event-rules">Event Rules</label>
+                                        <textarea name="event-rules" id="event-rules" className="form-control description" rows="5"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row dflex justify-content-end">
+                                <div className="col-2 mb-5">
+                                    <button type="submit" className="btn bg-orange pl-4 pr-4 mb-5 radius-10">ADD</button>
+                                </div>
+                            </div>
+                            </>
+                        }
                 </form>
             </div>
         </div>
-    )
+    );
 }
